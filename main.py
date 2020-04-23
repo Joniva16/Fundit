0ca699c697b2d706d3649d83147fbec39f1b7fb8
farhag29 <64219209+farhag29@users.noreply.github.com>
2020-04-23 15:24:58 -0400

    i

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from stockprice import fetchsp
 <<<<<<< HEAD
 #im-in
 =======
+import
+
+user_list_info = {name:'', password:'', capital:'', email:''}
+
+class User_info(object):
+    def User_info(self, name, password, capital, email):
+    #function to determine main char of a user
+        self.name = n
+        self.email = e
+        self.capital = c
+        self.password = p
+    #add value to the keys
 
 
 >>>>>>> 8c654ef4cea6437c47e847691914ee9d8002c564
