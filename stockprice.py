cf6f45d55148246ea11a081790b9d29ece5d9a2b
Jonathan Ivanov <jonathanivanov@gmail.com>
2020-04-23 10:03:21 -0700

    Create stockprice.py

---
+++ stockprice.py
@@ -0,0 +1,15 @@
+import yfinance as yf 
+import pandas as pd
+
+#This is a function to fetch the stock price
+#bstock is a reference to the bought stock
+#bstockh is a reference to the bought stock's history
+#bstockp is a reference to the bought stock's price
+
+def fetch(sticker):
+	bstock = yf.Ticker(sticker)
+	bstockh = bstock.history(period="1d")
+	bstockp = float(bstockh.iat[0,1])
+	print(bstockp)
+
+fetch("TSLA")
