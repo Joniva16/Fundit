e8ac291fe1bb78475c7ea0e5dbfe9b236142d753
Jonathan Ivanov <jonathanivanov@gmail.com>
2020-05-23 11:28:36 -0700

    new gui updates

--- gui.py
+++ gui.py
@@ -1,40 +1,17 @@
 from tkinter import *
 
-root = Tk()
-root.geometry("700x400")
-root.title("Fundit")
-root.configure(background="white")
+def credentials():
+	log = Tk()
+	log.geometry("500x600")
+	log.title("Fundit")
 
-frame_input= Frame(root,width=500,height=500,background="white")
-frame_input.place(x=0,y=0)
+	#Displaying the main logo on the sign in page
+	logo = Image.open("logo.png")
+	logo_render = ImageTk.Photoimage(logo)
+	lbl_logo = Label(log, image=logo_render)
+	lbl_logo.image = logo_render
+	lbl_logo.pack(fill=X)
 
-lbl_time = Label(frame_input,text="What is your username?", font=(13),background="white")
-lbl_time.place(x=10,y=10)
+	log.mainloop()
 
-entry_time = Entry(frame_input,bg="white",width=25)
-entry_time.place(x=200,y=10)
-
-frame_input2= Frame(root,width=500,height=400,background="white")
-frame_input2.place(x=0,y=0)
-
-lbl_time4 = Label(frame_input2,text="What is your password?", font=(13),background="white")
-lbl_time4.place(x=10,y=60)
-
-entry_time5 = Entry(frame_input2,bg="white",width=25)
-entry_time5.place(x=200,y=60)
-
-lbl_time4 = Label(frame_input2,text="What is your capital?", font=(13),background="white")
-lbl_time4.place(x=10,y=110)
-
-entry_time5 = Entry(frame_input2,bg="white",width=25)
-entry_time5.place(x=200,y=110)
-
-lbl_time4 = Label(frame_input2,text="What is your email?", font=(13),background="white")
-lbl_time4.place(x=10,y=160)
-
-entry_time5 = Entry(frame_input2,bg="white",width=25)
-entry_time5.place(x=200,y=160)
-
-
-
-root.mainloop()
+credentials()
